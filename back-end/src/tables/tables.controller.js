a45e467cc9d7d6722b3c5e11fad29cb1ce6c75e1
Tina Johnson <tmjohnson112341@gmail.com>
2022-06-03 09:44:19 -0400

    US-05

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -2,9 +2,7 @@ const service = require("./tables.service");
 const reservationService = require("../reservations/reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const hasProperties = require("../errors/hasProperties");
-
 const hasRequiredProperties = hasProperties("table_name", "capacity");
-
 const VALID_PROPERTIES = [
   "table_name",
   "capacity",
@@ -13,13 +11,11 @@ const VALID_PROPERTIES = [
   "created_at",
   "updated_at",
 ];
-
 function hasOnlyValidProperties(req, res, next) {
   const { data = {} } = req.body;
   const invalidFields = Object.keys(data).filter(
     (field) => !VALID_PROPERTIES.includes(field)
   );
-
   if (invalidFields.length) {
     return next({
       status: 400,
@@ -28,7 +24,6 @@ function hasOnlyValidProperties(req, res, next) {
   }
   next();
 }
-
 function validCapacity(req, res, next) {
   const { data } = req.body;
   if (typeof data["capacity"] !== "number" || data["capacity"] < 1) {
@@ -39,7 +34,6 @@ function validCapacity(req, res, next) {
   }
   next();
 }
-
 function validName(req, res, next) {
   const { data } = req.body;
   if (data["table_name"].length < 2) {
@@ -50,7 +44,6 @@ function validName(req, res, next) {
   }
   next();
 }
-
 async function reservationExists(req, res, next) {
   const { reservation_id } = req.body.data;
   const foundReservation = await reservationService.read(reservation_id);
@@ -64,17 +57,14 @@ async function reservationExists(req, res, next) {
     });
   }
 }
-
 async function list(req, res) {
   const data = await service.list();
   res.json({ data });
 }
-
 async function create(req, res) {
   const data = await service.create(req.body.data);
   res.status(201).json({ data });
 }
-
 async function tableExists(req, res, next) {
   const { table_id } = req.params;
   const foundTable = await service.read(table_id);
@@ -84,12 +74,11 @@ async function tableExists(req, res, next) {
   } else {
     next({
       status: 404,
-      message: "Table Could Not Be Found.",
+      message: `Table ${table_id} Could Not Be Found.`,
     });
   }
 }
-
-function capcityCheck(req, res, next) {
+function capacityCheck(req, res, next) {
   if (res.locals.table.capacity < res.locals.reservation.people) {
     next({
       status: 400,
@@ -101,10 +90,19 @@ function capcityCheck(req, res, next) {
 
 function occupiedCheck(req, res, next) {
   if (res.locals.table.reservation_id) {
-    console.log("We've hit this twice");
     return next({
       status: 400,
       message: "Table is occupied",
+    });
+  }
+  next();
+}
+
+function currentlyOccupied(req,res,next){
+  if (!res.locals.table.reservation_id) {
+    return next({
+      status: 400,
+      message: "Table is currently not occupied",
     });
   }
   next();
@@ -122,6 +120,12 @@ async function update(req, res) {
   res.json({ data });
 }
 
+async function finish(req, res) {
+  const table = res.locals.table;
+  const data = await service.finish(table.table_id, table.reservation_id);
+  res.json({ data });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
   create: [
@@ -136,8 +140,9 @@ module.exports = {
     hasOnlyValidProperties,
     hasProperties("reservation_id"),
     asyncErrorBoundary(reservationExists),
-    capcityCheck,
+    capacityCheck,
     occupiedCheck,
     asyncErrorBoundary(update),
   ],
+  finish: [asyncErrorBoundary(tableExists), currentlyOccupied, asyncErrorBoundary(finish)],
 };
